8373d7436dade2b3c6911d2f2f4067aeda257298
B-DU-DE <thebairdboyMatthew@gmail.com>
2025-06-23 12:34:50 +1000

    YIPEEE

--- Assets/Scripts/AntEntity.cs
+++ Assets/Scripts/AntEntity.cs
@@ -2,15 +2,56 @@ using UnityEngine;
 
 public class AntEntity : MonoBehaviour
 {
+    public AntScriptableObject antConfig;
+    private float health;
+    private float attack;
+    private float speed;
+    private AlliedFaction faction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
+        health = antConfig.health;
+        attack = antConfig.attack;
+        speed = antConfig.speed;
+    }
+
+    void Update() //Temp update to test some stuff
+    {
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            DamageAnt(1);
+            Debug.Log("Damaged ant for 1, this is its current health " + health);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public float getSpeed()
     {
+        //float newSpeed = speed * faction.speedMult
+        return speed;
+    }
+    public float getHealth()
+    {
+        //float newHealth = health * faction.healthMult
+        return health;
+    }
+    public float getAttack()
+    {
+        //float newAttack = attack * faction.attackMult
+        return attack;
+    }
+    public void DamageAnt(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            //ant would die here
+            Debug.Log("IM DEAD");
+        }
+
         
     }
+
+    
 }
